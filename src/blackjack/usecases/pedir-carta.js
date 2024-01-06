81e9fb31c8e05b1da679e478874b4deafb3e4e5c
Kelvin <Kelvin_Miliano@hotmail.com>
2024-01-05 20:49:56 -0400

    first commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @param {Array<String>} deck 
+ * @returns 
+ */
+// Esta función me permite tomar una carta
+export const pedirCarta = (deck) => {
+
+
+    if ( deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
